92b2df0d990da359a4bc4c76fa02d70e179fe42f
Ynigo Nino Cala Ramas <ramasynigo@gmail.com>
2025-03-14 15:52:32 +0800

    [components] Add initial Vue application routes

---
+++ components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const year = new Date().getFullYear();
+</script>
+<template>
+<footer>
+<nav>
+<a href="terms">Terms and Conditions</a>
+<span>&nbsp;|&nbsp;</span>
+<a href="privacy">Privacy Policy</a>
+<span>&nbsp;|&nbsp;</span>
+<a href="support">Support</a>
+</nav>
+<p>
+<span>&copy;&nbsp;</span>
+<span>{{ year }}</span>
+<span>&nbsp;Company Name. All rights reserved.</span>
+</p>
+</footer>
+</template>
+<style scoped></style>
---
+++ pages/policy.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const title = ref("impactful_games | Home");
+const description = ref("Game catalouge detailing various games with interactive features.");
+useSeoMeta({
+ title: () => title.value,
+ description: () => `${title.value} - ${description.value}`,
+ charset: "utf-8",
+ viewport: "width=device-width, initial-scale=1.0",
+ robots: "index, follow",
+ ogTitle: () => title.value,
+ ogDescription: () => `${title.value} - ${description.value}`,
+ // ogImage: "https://impactful_games.vercel.app/featured.jpg",
+ // twitterCard: "summary_large_image",
+});
+</script>
+
+<template>
+    <Html lang="en">
+    <Head>
+    <Link rel="icon" type="image/png" href="logo.png" />
+    <link rel="stylesheet" href="/reset.css" />
+    </Head>
+    </Html>
+   
+    <client-only>
+    <Welcome />
+    </client-only>
+   </template>
+   <style scoped></style>
