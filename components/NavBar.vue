13531de7d56a398169a0803f237f129120b95c5d
Xyryll Jay Taneo <xyrylljay@gmail.com>
2025-04-11 16:13:33 +0800

    [UI] Add NavBar component with design and usage

---
+++ components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <client-only>
+    <header class="ig-navbar">
+    <nav class="ig-nav-left">
+    <a href="/" class="ig-nav-home">
+    <img src="/joystick.png" width=20 height=20 class="ig-nav-logo">
+    <span>ImpactfulGames</span>
+    </a>
+    </nav>
+    <nav class="ig-nav-right">
+    <ul>
+    <li><a href="#" class="ig-nav-link">Browse Games by Genre</a></li>
+    <li><a href="#" class="ig-nav-link">View Game Details</a></li>
+    <li><a href="#" class="ig-nav-link">User Authentication</a></li>
+    </ul>
+</nav>
+ </header>
+ </client-only>
+</template>
