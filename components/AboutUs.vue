85504181cc9ef4727d255b6690c1c42faa819ae8
Kissu1 <ricoposas@gmail.com>
2025-05-09 23:15:53 +0800

    [style] refactor style to be uniform

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -6,18 +6,3 @@
   </div>
 </template>
 
-<style scoped>
-.about-link-footer {
-  width: 100%;
-  padding: 10px 0;
-  font-size: 0.8em;
-  text-align: center;
-  color: #666;
-}
-
-.about-footer-links a {
-  font-weight: bold;
-  text-decoration: none;
-  color: #666;
-}
-</style>
